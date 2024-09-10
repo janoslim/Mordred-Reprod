923bf24ef75a4e5d2314e7448d6ea06552264d78
Chaemin Lim <vn.cmlim@gmail.com>
2024-09-10 16:20:47 +0900

    add test dir

--- mordred-transform.py
+++ mordred-transform.py
@@ -13,14 +13,6 @@ class cd:
     def __exit__(self, etype, value, traceback):
         os.chdir(self.savedPath)
 
-def transform(scale_factor):
-    path = './'
-    ip = '/mnt/hdd/ssb-dbgen-{}/'.format (scale_factor) 
-    op = '/mnt/hdd/ssb-dbgen-{}/transformed/'.format (scale_factor) 
-    with cd(path):
-        os.system('mkdir -p %s' % op)
-        os.system('python3 convert.py {}'.format(ip))
-        os.system('./loader --lineorder %s/lineorder.tbl --ddate %s/date.tbl --customer %s/customer.tbl.p --supplier %s/supplier.tbl.p --part %s/part.tbl.p --datadir %s' % (ip, ip, ip, ip, ip, op))
 
 
 linecounts = {}
@@ -34,11 +26,29 @@ linecounts[160] = 960017389
 linecounts[180] = 1080017552
 linecounts[200] = 1200018434
 
+def transform(scale_factor):
+    path = './'
+    ip = '/mnt/hdd3/ssb-dbgen-{}/'.format (scale_factor) 
+    op = '/mnt/hdd3/ssb-dbgen-{}/transformed/'.format (scale_factor) 
+    with cd(path):
+        os.system('mkdir -p %s' % op)
+        os.system('python3 convert.py {}'.format(ip))
+        os.system('./loader --lineorder %s/lineorder.tbl --ddate %s/date.tbl --customer %s/customer.tbl.p --supplier %s/supplier.tbl.p --part %s/part.tbl.p --datadir %s' % (ip, ip, ip, ip, ip, op))
+
+def linecount(scale_factor):
+    path = '/mnt/hdd3/ssb-dbgen-{}/transformed/'.format (scale_factor) 
+    with cd(path):
+        os.system('wc -l lineorder.tbl')
+        os.system('wc -l part.tbl')
+        os.system('wc -l supplier.tbl')
+        os.system('wc -l customer.tbl')
+        os.system('wc -l date.tbl')
+        
 def sort(scale_factor):
     path = './'
-    ip = '/mnt/hdd/ssb-dbgen-{}/'.format (scale_factor) 
-    op = '/mnt/hdd/ssb-dbgen-{}/transformed/'.format (scale_factor) 
-    sortop = '/mnt/hdd/ssb-dbgen-{}/columnar/'.format (scale_factor) 
+    ip = '/mnt/hdd3/ssb-dbgen-{}/'.format (scale_factor) 
+    op = '/mnt/hdd3/ssb-dbgen-{}/transformed/'.format (scale_factor) 
+    sortop = '/mnt/hdd3/ssb-dbgen-{}/columnar/'.format (scale_factor) 
     with cd(path):
         os.system ("./columnSort {}/LINEORDER {}/LINEORDERSORT 5 16 {}".format (op, op, linecounts[scale_factor]))
 
@@ -46,13 +56,16 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = 'data gen')
     # parser.add_argument('--dataset', type=str, choices=['ssb'])
     parser.add_argument('--sf', type=int)
+    parser.add_argument('--action', type=str)
     # parser.add_argument('action', type=str, choices=['gen', 'transform'])
     args = parser.parse_args()
 
-    # elif args.action == 'transform':
-    # transform(args.sf)
-    sort (args.sf)
-
+    if args.action == 'transform':
+        transform(args.sf)
+    elif args.action == 'linecount':
+        linecount (args.sf)
+    elif args.action == 'sort':
+        sort (args.sf)
 
 '''
 for i in 40 60 80 100 120 140 160 180 200;
